0fbbc8989d7f42e321880adf3796794534850f55
YingzhePeng <yingzhepeng@foxmail.com>
2023-09-06 19:07:48 +0800

    init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,3 @@
 from .idx_base_caption_iclm import IdxBaseCaptionICLM
 from .sen_img_caption_iclm import SenImgEncodeCaptionICLM
+from .sen_img_caption_iclm_add_image_info import SenImgEncodeAddImageCaptionICLM
