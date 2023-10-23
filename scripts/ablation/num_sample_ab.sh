f7851601fc77705a4a6c5e6e5d6c69a656ca46c8
ForJadeForest <yingzhepeng@foxmail.com>
2023-10-23 23:58:09 +0800

    update

--- scripts/ablation/num_sample_ab.sh
+++ scripts/ablation/num_sample_ab.sh
@@ -25,9 +25,9 @@ run_vqa_train() {
     local data_file=$1
     local val_step=$2
     local ex_name_suffix=$3
-    local ex_name_prefix="ab_${task}"
+    local ex_name_prefix="ab_vqa"
 
-
+    iclm_model="query_img_text_ice_img_text"
     echo "==========Begin: ${ex_name_prefix}_${ex_name_suffix}-ICLM: ${iclm_model}==========" 
     python train.py train="query_img_text_ice_img_text" \
         data_files="${data_file}" \
@@ -40,6 +40,12 @@ run_vqa_train() {
 }
 
 run_caption_train(){
+    local data_file=$1
+    local val_step=$2
+    local ex_name_suffix=$3
+    local ex_name_prefix="ab_caption"
+    iclm_model="query_img_ice_img_text"
+    
     echo "==========Begin: ${ex_name_prefix}_${ex_name_suffix}-ICLM: ${iclm_model}==========" 
     python train.py train="query_img_ice_img_text" \
         data_files="${data_file}" \
@@ -57,24 +63,25 @@ run_caption_train(){
 
 run_inference() {
     local ex_name_suffix=$1
-    local ex_name_prefix="ab_${task}"
     local task=$2
+    local ex_name_prefix="ab_${task}"
+    
     if [ "${task}" == "vqa" ]; then
+        iclm_model="query_img_text_ice_img_text"
         echo "==========Begin: ${ex_name_prefix}_${ex_name_suffix}-ICLM: ${iclm_model}==========" 
         python inference_flamingo_fast.py   train="${iclm_model}" \
                                             ex_name="${ex_name_prefix}_${ex_name_suffix}_${iclm_model}" \
                                             dataset=vqav2_local_sub \
                                             task=${task} \
-                                            inference_bs=${inference_bs} \
                                             test_iclm=true
 
     elif [ "${task}" == "caption" ]; then
         echo "==========Begin: ${ex_name_prefix}_${ex_name_suffix}-ICLM: ${iclm_model}==========" 
+        iclm_model="query_img_ice_img_text"
         python inference_flamingo_fast.py   train="${iclm_model}" \
-                                            ex_name="${ex_name_prefix}_${ex_name_suffix}_non_norm_freeze_adapter_${iclm_model}" \
+                                            ex_name="${ex_name_prefix}_${ex_name_suffix}_freeze_adapter_non_norm_${iclm_model}" \
                                             dataset=coco2017 \
                                             task=${task}\
-                                            inference_bs=${inference_bs}\
                                             test_iclm=true\
                                             train.iclm_model.norm=false \
                                             train.iclm_model.freeze_prefix_list="[img_model,sen_model]" \
@@ -82,35 +89,41 @@ run_inference() {
     fi
 }
 
-
-run_caption_train "caption-coco2017-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:1000.json" 80 "1kanchors"
-run_caption_train "caption-coco2017-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:2000.json" 80 "2kanchors"
-run_caption_train "caption-coco2017-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:3000.json" 80 "3kanchors"
-run_caption_train "caption-coco2017-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:4000.json" 80 "4kanchors"
-
-
+# export CUDA_VISIBLE_DEVICES="0,1"
+# run_caption_train "caption-coco2017-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:1000.json" 80 "1kanchors" && \
+# run_caption_train "caption-coco2017-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:2000.json" 80 "2kanchors" && \
+# run_caption_train "caption-coco2017-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:3000.json" 80 "3kanchors" && \
+# run_caption_train "caption-coco2017-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:4000.json" 80 "4kanchors" &
 
 
 
-run_vqa_train "vqa-vqav2-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:1000.json" 80 "1kanchors"
-run_vqa_train "vqa-vqav2-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:2000.json" 80 "2kanchors"
-run_vqa_train "vqa-vqav2-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:3000.json" 80 "3kanchors"
-run_vqa_train "vqa-vqav2-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:4000.json" 80 "4kanchors"
 
+# export CUDA_VISIBLE_DEVICES="2,3"
+# run_vqa_train "vqa-vqav2-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:1000.json" 80 "1kanchors" && \
+# run_vqa_train "vqa-vqav2-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:2000.json" 80 "2kanchors" && \
+# run_vqa_train "vqa-vqav2-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:3000.json" 80 "3kanchors" && \
+# run_vqa_train "vqa-vqav2-only_y_loss-OpenFlamingo-9B-vitl-mpt7b-random-beam_size:5-few_shot:2-candidate_set_num:64-sample_num:4000.json" 80 "4kanchors" & 
+# wait
 
-export CUDA_VISIBLE_DEVICES="0" && run_inference "1kanchors" "vqa" &
-sleep 120
-export CUDA_VISIBLE_DEVICES="1" && run_inference "2kanchors" "vqa" &
-sleep 120
-export CUDA_VISIBLE_DEVICES="2" && run_inference "3kanchors" "vqa" &
-sleep 120
-export CUDA_VISIBLE_DEVICES="3" && run_inference "4kanchors" "vqa" &
 
 
 export CUDA_VISIBLE_DEVICES="0" && run_inference "1kanchors" "caption" &
-sleep 120
+sleep 60
 export CUDA_VISIBLE_DEVICES="1" && run_inference "2kanchors" "caption" &
-sleep 120
+sleep 60
 export CUDA_VISIBLE_DEVICES="2" && run_inference "3kanchors" "caption" &
-sleep 120
+sleep 60
 export CUDA_VISIBLE_DEVICES="3" && run_inference "4kanchors" "caption" &
+
+wait 
+
+
+export CUDA_VISIBLE_DEVICES="0" && run_inference "1kanchors" "vqa" &
+sleep 60
+export CUDA_VISIBLE_DEVICES="1" && run_inference "2kanchors" "vqa" &
+sleep 60
+
+export CUDA_VISIBLE_DEVICES="2" && run_inference "3kanchors" "vqa" &
+sleep 60
+export CUDA_VISIBLE_DEVICES="3" && run_inference "4kanchors" "vqa" &
+wait
