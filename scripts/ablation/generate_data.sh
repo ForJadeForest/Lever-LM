2038e1e157a1397f186e6ba8bc44aefe3b5d4841
YingzhePeng <yingzhepeng@foxmail.com>
2023-10-28 12:50:23 +0000

    update

--- scripts/ablation/generate_data.sh
+++ scripts/ablation/generate_data.sh
@@ -5,102 +5,43 @@
 task=${1:-caption}
 dataset=${2:-coco2017}
 gpu_ids=${3:-"[0]"}
+flamingo=${4:-"flamingo_9B"}
 
 if [ -z "$task" ] || [ -z "$dataset" ]; then
     echo "Usage: $0 <task> <dataset> [gpu_ids]"
     exit 1
 fi
 
-
 which python
 
+declare -A configs
+
 # Baseline:
-echo "=====================BEGIN TO BASELINE====================="
-python generate_data.py \
-    beam_size=5 \
-    few_shot_num=2 \
-    candidate_set_num=64 \
-    sample_num=5000 \
-    candidate_set_method="random" \
-    gpu_ids="${gpu_ids}" \
-    task=${task} \
-    dataset=${dataset}
+configs["BASELINE"]="beam_size=5 candidate_set_num=64"
 
-echo "=====================BEGIN TO BEAMSIZE:1====================="
-python generate_data.py \
-    beam_size=1 \
-    few_shot_num=2 \
-    candidate_set_num=64 \
-    sample_num=5000 \
-    candidate_set_method="random" \
-    gpu_ids="${gpu_ids}" \
-    task=${task} \
-    dataset=${dataset}
+# Different beam sizes:
+configs["BEAMSIZE:1"]="beam_size=1 candidate_set_num=64"
+configs["BEAMSIZE:10"]="beam_size=10 candidate_set_num=64"
 
-echo "=====================BEGIN TO BEAMSIZE:10====================="
-python generate_data.py \
-    beam_size=10 \
-    few_shot_num=2 \
-    candidate_set_num=64 \
-    sample_num=5000 \
-    candidate_set_method="random" \
-    gpu_ids="${gpu_ids}" \
-    task=${task} \
-    dataset=${dataset}
+# Different candidate set nums:
+configs["CANDIDATE:32"]="beam_size=5 candidate_set_num=32"
+configs["CANDIDATE:128"]="beam_size=5 candidate_set_num=128"
 
+# Different candidate set methods:
+configs["TEXT-SIM"]="beam_size=5 candidate_set_num=64 candidate_set_method=text-sim"
+configs["IMAGE-SIM"]="beam_size=5 candidate_set_num=64 candidate_set_method=image-sim"
 
-echo "=====================BEGIN TO CANDIDATE:32====================="
-python generate_data.py \
-    beam_size=5 \
-    few_shot_num=2 \
-    candidate_set_num=32 \
-    sample_num=5000 \
-    candidate_set_method="random" \
-    gpu_ids="${gpu_ids}" \
-    task=${task} \
-    dataset=${dataset}
+# Different sample nums:
+configs["1w SAMPLE_NUM"]="beam_size=5 candidate_set_num=64 sample_num=10000"
 
-
-echo "=====================BEGIN TO CANDIDATE:128====================="
-python generate_data.py \
-    beam_size=5 \
-    few_shot_num=2 \
-    candidate_set_num=128 \
-    sample_num=5000 \
-    candidate_set_method="random" \
-    gpu_ids="${gpu_ids}" \
-    task=${task} \
-    dataset=${dataset}
-
-echo "=====================BEGIN TO TEXT-SIM====================="
-python generate_data.py \
-    beam_size=5 \
-    few_shot_num=2 \
-    candidate_set_num=64 \
-    sample_num=5000 \
-    candidate_set_method="text-sim" \
-    gpu_ids="${gpu_ids}" \
-    task=${task} \
-    dataset=${dataset}
-
-echo "=====================BEGIN TO IMAGE-SIM====================="
-python generate_data.py \
-    beam_size=5 \
-    few_shot_num=2 \
-    candidate_set_num=64 \
-    sample_num=5000 \
-    candidate_set_method="image-sim" \
-    gpu_ids="${gpu_ids}" \
-    task=${task} \
-    dataset=${dataset}
-
-echo "=====================BEGIN TO 1w SAMPLE_NUM===================="
-python generate_data.py \
-    beam_size=5 \
-    few_shot_num=2 \
-    candidate_set_num=64 \
-    sample_num=10000 \
-    candidate_set_method="random" \
-    gpu_ids="${gpu_ids}" \
-    task=${task} \
-    dataset=${dataset}
+for key in "${!configs[@]}"; do
+    echo "=====================BEGIN TO $key====================="
+    python generate_data.py \
+        ${configs[$key]} \
+        few_shot_num=2 \
+        sample_num=5000 \
+        gpu_ids="${gpu_ids}" \
+        task=${task} \
+        dataset=${dataset} \
+        flamingo=${flamingo}
+done
