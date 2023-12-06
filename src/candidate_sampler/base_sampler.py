d7536d56009b0273ea5abdd71c2a4edd766b747b
YingzhePeng <yingzhepeng@foxmail.com>
2023-12-06 11:46:17 +0800

    init sampler

---
+++ src/candidate_sampler/base_sampler.py
@@ -0,0 +1,52 @@
+import json
+import os
+from typing import Any
+
+from loguru import logger
+
+
+class BaseSampler:
+    def __init__(
+        self, candidate_num, sampler_name, cache_dir, overwrite, other_info=''
+    ) -> None:
+        self.candidate_num = candidate_num
+        self.sampler_name = sampler_name
+        self.cache_dir = cache_dir
+        if not os.path.exists(self.cache_dir):
+            os.makedirs(self.cache_dir)
+        self.overwrite = overwrite
+        self.cache_file = os.path.join(
+            self.cache_dir,
+            f"{self.sampler_name}-{other_info}:{self.candidate_num}.json",
+        )
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        data = self.load_cache_file()
+        if data is not None:
+            return data
+        data = self.sample(*args, **kwargs)
+        self.save_cache_file(data)
+        return data
+
+    def sample(self, *args, **kwargs):
+        raise NotImplemented
+
+    def load_cache_file(self):
+        if not os.path.exists(self.cache_file) or self.overwrite:
+            logger.info(
+                f'the candidate set cache {self.cache_file} not exists or set overwrite mode. (overwrite: {self.overwrite})'
+            )
+            return
+        else:
+            logger.info(
+                f'the candidate set cache {self.cache_file} exists, reloding...'
+            )
+            with open(self.cache_file, 'r') as f:
+                data = json.load(f)
+            data = {int(k): v for k, v in data.items()}
+            return data
+
+    def save_cache_file(self, data):
+        with open(self.cache_file, 'w') as f:
+            logger.info(f'save the candidate data to {self.cache_file}')
+            json.dump(data, f)
---
+++ src/candidate_sampler/mix_sampler.py
@@ -0,0 +1,91 @@
+import os
+import random
+
+import torch
+from loguru import logger
+
+from src.utils import encode_text, recall_sim_feature
+
+from .base_sampler import BaseSampler
+from .img_sim_sampler import ImgSimSampler
+from .random_sampler import RandSampler
+from .text_sim_sampler import TextSimSampler
+
+
+class MixSimSampler(BaseSampler):
+    def __init__(
+        self,
+        candidate_num,
+        sampler_name,
+        cache_dir,
+        overwrite,
+        clip_model_name,
+        feature_cache_filename,
+        text_field_name,
+        img_field_name,
+        device,
+        candidate_set_encode_bs,
+        sampler_ratio: dict,
+    ):
+        self.sampler_ratio = sampler_ratio
+        self.sampler_candidate_num = {}
+        for i, (n, r) in enumerate(self.sampler_ratio.items()):
+            if i == len(self.sampler_ratio) - 1:
+                self.sampler_candidate_num[n] = candidate_num - sum(
+                    [v for v in self.sampler_candidate_num.values()]
+                )
+            else:
+                self.sampler_candidate_num[n] = int(r * candidate_num)
+        other_info = (
+            f'Rand:{self.sampler_candidate_num["RandSampler"]}-'
+            f'Text:{self.sampler_candidate_num["TextSimSampler"]}-'
+            f'Img:{self.sampler_candidate_num["ImgSimSampler"]}'
+        )
+        super().__init__(
+            candidate_num=candidate_num,
+            sampler_name=sampler_name,
+            cache_dir=cache_dir,
+            overwrite=overwrite,
+            other_info=other_info,
+        )
+        self.rand_sampler = RandSampler(
+            candidate_num=self.sampler_candidate_num['RandSampler'],
+            sampler_name=sampler_name,
+            cache_dir=cache_dir,
+            overwrite=overwrite,
+        )
+        self.text_sim_sampler = TextSimSampler(
+            candidate_num=self.sampler_candidate_num['TextSimSampler'],
+            sampler_name=None,
+            cache_dir=cache_dir,
+            overwrite=overwrite,
+            clip_model_name=clip_model_name,
+            feature_cache_filename=feature_cache_filename,
+            text_field_name=text_field_name,
+            device=device,
+            candidate_set_encode_bs=candidate_set_encode_bs,
+        )
+        self.img_sim_sampler = ImgSimSampler(
+            candidate_num=self.sampler_candidate_num['ImgSimSampler'],
+            sampler_name=None,
+            cache_dir=cache_dir,
+            overwrite=overwrite,
+            clip_model_name=clip_model_name,
+            feature_cache_filename=feature_cache_filename,
+            img_field_name=img_field_name,
+            device=device,
+            candidate_set_encode_bs=candidate_set_encode_bs,
+        )
+
+    @torch.inference_mode()
+    def sample(self, anchor_set, train_ds):
+        final_res = {k: [] for k in anchor_set}
+        rand_res = self.rand_sampler.sample(anchor_set, train_ds)
+        img_sim_res = self.img_sim_sampler.sample(anchor_set, train_ds)
+        text_sim_res = self.text_sim_sampler.sample(anchor_set, train_ds)
+        for k in anchor_set:
+            final_res[k].extend(rand_res[k])
+            final_res[k].extend(img_sim_res[k])
+            final_res[k].extend(text_sim_res[k])
+
+        return final_res
