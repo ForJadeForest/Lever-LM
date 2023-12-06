6bb7e4321d672f7868832672a855554655274413
YingzhePeng <yingzhepeng@foxmail.com>
2023-12-06 16:45:09 +0800

    update

--- scripts/generate_data.sh
+++ scripts/generate_data.sh
@@ -2,11 +2,6 @@ task=${1:-caption}
 dataset=${2:-coco2017}
 gpu_ids=${3:-"[0]"}
 
-if [ -z "$task" ] || [ -z "$dataset" ]; then
-    echo "Usage: $0 <task> <dataset> [gpu_ids]"
-    exit 1
-fi
-
 
 python generate_data.py beam_size=5 \
                         sampler.candidate_num=64 \
