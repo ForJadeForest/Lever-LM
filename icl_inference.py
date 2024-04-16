92365d4dba9ff295f494b2f105186d850a0b8b10
ForJadeForest <yingzhepeng@foxmail.com>
2024-04-16 10:29:17 +0800

    change ' to "

--- icl_inference.py
+++ icl_inference.py
@@ -23,21 +23,21 @@ from utils import caption_postprocess, load_ds, vqa_postprocess
 def get_icd_lm_path(cfg):
     if cfg.icd_lm_path is None:
         logger.info(
-            f'detect icd_lm_path is None, now try to find in {cfg.result_dir}/model_cpk/{cfg.ex_name}'
+            f"detect icd_lm_path is None, now try to find in {cfg.result_dir}/model_cpk/{cfg.ex_name}"
         )
         cpk_dir = os.path.join(
-            cfg.result_dir, 'model_cpk', cfg.task.task_name, cfg.ex_name
+            cfg.result_dir, "model_cpk", cfg.task.task_name, cfg.ex_name
         )
         cpk_list = []
         for f in os.listdir(cpk_dir):
             cpk_list.append(os.path.join(cpk_dir, f))
         cpk_list = list(filter(lambda x: cfg.default_cpk_key in x, cpk_list))
         if cpk_list:
-            logger.info(f'Detect {cpk_list[0]}, now begin to load cpk...')
+            logger.info(f"Detect {cpk_list[0]}, now begin to load cpk...")
             icd_lm_path = cpk_list[0]
         else:
             raise ValueError(
-                f'The icd_lm_path is None and detect no checkpoint can use in {cpk_dir}'
+                f"The icd_lm_path is None and detect no checkpoint can use in {cpk_dir}"
             )
     else:
         icd_lm_path = cfg.icd_lm_path
@@ -46,8 +46,8 @@ def get_icd_lm_path(cfg):
 
 def init_icd_lm(cfg, icd_lm_path):
     icd_lm = hydra.utils.instantiate(cfg.train.icd_lm)
-    state_dict = torch.load(icd_lm_path)['state_dict']
-    state_dict = {k.replace('icd_lm.', ''): v for k, v in state_dict.items()}
+    state_dict = torch.load(icd_lm_path)["state_dict"]
+    state_dict = {k.replace("icd_lm.", ""): v for k, v in state_dict.items()}
     icd_lm.load_state_dict(state_dict)
     processor = AutoProcessor.from_pretrained(cfg.train.icd_lm.clip_name)
     return icd_lm, processor
@@ -56,10 +56,10 @@ def init_icd_lm(cfg, icd_lm_path):
 def record(result_json_path: str, new_data: dict):
     recorded_data = {}
     if os.path.exists(result_json_path):
-        with open(result_json_path, 'r') as f:
+        with open(result_json_path, "r") as f:
             recorded_data = json.load(f)
 
-    with open(result_json_path, 'w') as f:
+    with open(result_json_path, "w") as f:
         recorded_data.update(new_data)
         json.dump(recorded_data, f, indent=4)
 
@@ -78,12 +78,12 @@ def evaluate_retriever(
     info = base_info + retriever_name
     for shot_num in shot_num_list:
         logger.info(
-            f'Now begin test {cfg.task.task_name}: {retriever_name} with {shot_num=}'
+            f"Now begin test {cfg.task.task_name}: {retriever_name} with {shot_num=}"
         )
-        output_files = info + f'-bs:{cfg.inference_bs}-{shot_num=}'
+        output_files = info + f"-bs:{cfg.inference_bs}-{shot_num=}"
         icd_idx_list = retriever.retrieve(shot_num)
 
-        if cfg.task.task_name == 'caption':
+        if cfg.task.task_name == "caption":
             metric = inference_caption(
                 inferencer=inferencer,
                 ds=ds,
@@ -92,7 +92,7 @@ def evaluate_retriever(
                 output_json_filename=output_files,
                 model_name=cfg.lvlm.name,
             )
-        elif cfg.task.task_name == 'vqa':
+        elif cfg.task.task_name == "vqa":
             metric = inference_vqa(
                 inferencer=inferencer,
                 ds=ds,
@@ -102,44 +102,44 @@ def evaluate_retriever(
                 output_json_filename=output_files,
                 model_name=cfg.lvlm.name,
             )
-        retriever_res[f'{shot_num=}'] = metric
-        logger.info(f'{output_files}: {metric=}')
+        retriever_res[f"{shot_num=}"] = metric
+        logger.info(f"{output_files}: {metric=}")
         record(result_json_path, {info: retriever_res})
 
 
 def init_retriever(retriever_name, ds, cfg):
-    if retriever_name == 'ZeroShot':
-        return ZeroRetriever(ds['train'], ds['validation'])
-    elif retriever_name == 'RandomRetriever':
+    if retriever_name == "ZeroShot":
+        return ZeroRetriever(ds["train"], ds["validation"])
+    elif retriever_name == "RandomRetriever":
         return RandRetriever(
-            ds['train'],
-            ds['validation'],
+            ds["train"],
+            ds["validation"],
             seed=cfg.seed,
             fixed=cfg.random_retrieval_fixed,
         )
-    elif retriever_name.startswith('MMTopKRetriever'):
-        mode = retriever_name.split('-')[-1]
+    elif retriever_name.startswith("MMTopKRetriever"):
+        mode = retriever_name.split("-")[-1]
         index_field = (
             cfg.task.icd_text_feature_field
-            if mode.endswith('t')
+            if mode.endswith("t")
             else cfg.task.image_field
         )
         test_field = (
             cfg.task.image_field
-            if mode.startswith('i')
+            if mode.startswith("i")
             else cfg.task.icd_text_feature_field
         )
 
         cache_file = os.path.join(
             cfg.result_dir,
-            'cache',
+            "cache",
             f'{cfg.task.task_name}-{cfg.dataset.name}-{cfg.mmtopk_clip_name.split("/")[-1]}-{mode}-'
-            f'index_field:{index_field}-test_data_num:{cfg.test_data_num}-'
-            f'test_field:{test_field}-emb_cache.pth',
+            f"index_field:{index_field}-test_data_num:{cfg.test_data_num}-"
+            f"test_field:{test_field}-emb_cache.pth",
         )
         return MMTopkRetriever(
-            ds['train'],
-            ds['validation'],
+            ds["train"],
+            ds["validation"],
             mode=mode,
             index_field=index_field,
             test_field=test_field,
@@ -149,12 +149,12 @@ def init_retriever(retriever_name, ds, cfg):
             batch_size=32,
             num_workers=8,
         )
-    elif retriever_name == 'ICDLMRetriever':
+    elif retriever_name == "ICDLMRetriever":
         icd_lm_path = get_icd_lm_path(cfg)
         icd_lm, processor = init_icd_lm(cfg, icd_lm_path=icd_lm_path)
         return ICDLMRetriever(
-            ds['train'],
-            ds['validation'],
+            ds["train"],
+            ds["validation"],
             icd_lm=icd_lm,
             processor=processor,
             query_image_field=cfg.train.icd_lm_ds.query_image_field,
@@ -179,8 +179,8 @@ def inference_caption(
     model_name,
 ):
     output_dict = inferencer.inference(
-        train_ds=ds['train'],
-        test_ds=ds['validation'],
+        train_ds=ds["train"],
+        test_ds=ds["validation"],
         ice_idx_list=icd_idx_list,
         output_json_filename=output_json_filename,
     )
@@ -188,9 +188,9 @@ def inference_caption(
     for idx in output_dict:
         pred_coco.append(
             {
-                'image_id': output_dict[idx]['image_id'],
-                'caption': caption_postprocess(
-                    output_dict[idx]['prediction'], model_name
+                "image_id": output_dict[idx]["image_id"],
+                "caption": caption_postprocess(
+                    output_dict[idx]["prediction"], model_name
                 ),
             }
         )
@@ -208,8 +208,8 @@ def inference_vqa(
     model_name,
 ):
     output_dict = inferencer.inference(
-        train_ds=ds['train'],
-        test_ds=ds['validation'],
+        train_ds=ds["train"],
+        test_ds=ds["validation"],
         ice_idx_list=icd_idx_list,
         output_json_filename=output_json_filename,
     )
@@ -217,15 +217,15 @@ def inference_vqa(
     for idx in output_dict:
         preds.append(
             {
-                'answer': vqa_postprocess(
-                    output_dict[idx]['prediction'], model_name=model_name
+                "answer": vqa_postprocess(
+                    output_dict[idx]["prediction"], model_name=model_name
                 ),
-                'question_id': output_dict[idx]['question_id'],
+                "question_id": output_dict[idx]["question_id"],
             }
         )
     random_uuid = str(uuid.uuid4())
 
-    with open(f'{random_uuid}.json', 'w') as f:
+    with open(f"{random_uuid}.json", "w") as f:
         f.write(json.dumps(preds, indent=4))
     acc = compute_vqa_accuracy(f"{random_uuid}.json", val_ques_path, val_ann_path)
     # delete the temporary file
@@ -235,15 +235,15 @@ def inference_vqa(
 
 @hydra.main(version_base=None, config_path="./configs", config_name="inference.yaml")
 def main(cfg: DictConfig):
-    logger.info(f'{cfg=}')
+    logger.info(f"{cfg=}")
     result_dir = os.path.join(
         cfg.result_dir,
-        'icl_inference',
+        "icl_inference",
         cfg.lvlm.name,
         cfg.task.task_name,
         cfg.ex_name,
     )
-    result_json_path = os.path.join(result_dir, 'metrics.json')
+    result_json_path = os.path.join(result_dir, "metrics.json")
 
     test_data_num = cfg.test_data_num
     index_data_num = cfg.index_data_num
@@ -251,31 +251,31 @@ def main(cfg: DictConfig):
     ds = load_ds(cfg)
 
     if index_data_num != -1:
-        ds['train'] = ds['train'].select(
-            random.sample(range(len(ds['train'])), index_data_num)
+        ds["train"] = ds["train"].select(
+            random.sample(range(len(ds["train"])), index_data_num)
         )
     if test_data_num != -1:
-        ds['validation'] = ds['validation'].select(range(test_data_num))
+        ds["validation"] = ds["validation"].select(range(test_data_num))
 
     interface = init_lvlm(cfg, device=cfg.device)
 
     inferencer = VLICLInferecer(
         interface=interface,
-        train_ds=ds['train'],
-        test_ds=ds['validation'],
+        train_ds=ds["train"],
+        test_ds=ds["validation"],
         generation_kwargs=cfg.task.gen_args,
         other_save_field=cfg.task.other_save_field,
         num_workers=cfg.num_workers,
         num_proc=cfg.num_proc,
         batch_size=cfg.inference_bs,
-        output_json_filepath=os.path.join(result_dir, 'generation_metainfo'),
+        output_json_filepath=os.path.join(result_dir, "generation_metainfo"),
     )
 
-    base_info = f'{str(datetime.datetime.now())}-{test_data_num=}-'
+    base_info = f"{str(datetime.datetime.now())}-{test_data_num=}-"
 
     retriever_list = [
-        ('ZeroShot', [0] if cfg.test_zero_shot else []),
-        ('RandomRetriever', cfg.shot_num_list if cfg.test_random else []),
+        ("ZeroShot", [0] if cfg.test_zero_shot else []),
+        ("RandomRetriever", cfg.shot_num_list if cfg.test_random else []),
         (
             f'MMTopKRetriever-{cfg.mmtopk_clip_name.split("/")[-1]}-i2t',
             cfg.shot_num_list if cfg.test_i2t else [],
@@ -289,7 +289,7 @@ def main(cfg: DictConfig):
             cfg.shot_num_list if cfg.test_t2t else [],
         ),
         (
-            'ICDLMRetriever',
+            "ICDLMRetriever",
             cfg.shot_num_list if cfg.test_icd_lm else [],
         ),
     ]
@@ -320,6 +320,6 @@ def shuffle_2d_list(matrix):
     return new_matrix
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_dotenv()
     main()
