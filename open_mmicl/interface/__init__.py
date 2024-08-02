8e56760d1b70cd828977daa9f99b578f0177371c
YingzhePeng <yingzhepeng@foxmail.com>
2024-08-02 16:29:24 +0800

    Add IDEFICS2Interface to interface/__init__.py

--- open_mmicl/interface/__init__.py
+++ open_mmicl/interface/__init__.py
@@ -1,4 +1,5 @@
 from .base_interface import BaseInterface
 from .flamingo_interface import FlamingoInterface
+from .idefics2_interface import IDEFICS2Interface
 from .idefics_interface import IDEFICSInterface
 from .llm_interface import LLMInterface
